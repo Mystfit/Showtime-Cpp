295a1cba2c79fe056861076408af4fc31665fbcc
Byron Mallett <byronated@gmail.com>
2018-03-02 12:14:14 +1300

    Fixed double git clone in install_dependencies

--- install_dependencies.bat
+++ install_dependencies.bat
@@ -1,41 +1,37 @@
 ECHO ON
 REM ----------------------
 REM Environment variables
-IF NOT DEFINED BUILD_FOLDER set BUILD_FOLDER=.
+IF NOT DEFINED BUILD_FOLDER set BUILD_FOLDER=C:\projects\showtime-cpp
 
 REM Set up dependency directories
 pushd %BUILD_FOLDER%
 
-IF NOT DEFINED DEPENDENCY_DIR(
-    set DEPENDENCY_DIR=%BUILD_FOLDER%/dependencies
-)
+IF NOT DEFINED DEPENDENCY_DIR set DEPENDENCY_DIR=%BUILD_FOLDER%\dependencies
 IF NOT EXIST %DEPENDENCY_DIR% (
     mkdir "%DEPENDENCY_DIR%"
-    mkdir "%DEPENDENCY_DIR%/install"
+    mkdir "%DEPENDENCY_DIR%\install"
 )
-IF NOT DEFINED HUNTER_ROOT (
-    set HUNTER_ROOT=%DEPENDENCY_DIR%/hunter_root
-    mkdir "%HUNTER_ROOT%"
-)
+IF NOT DEFINED HUNTER_ROOT set HUNTER_ROOT=%DEPENDENCY_DIR%\hunter_root
+IF NOT EXIST %HUNTER_ROOT% mkdir "%HUNTER_ROOT%"
 
 REM Aquire patched hunterized CZMQ
 pushd "%DEPENDENCY_DIR%"
-IF NOT EXIST %DEPENDENCY_DIR%/czmq git clone https://github.com/mystfit/czmq.git
+IF NOT EXIST %DEPENDENCY_DIR%\czmq git clone https://github.com/mystfit/czmq.git
 
 pushd czmq
 git checkout hunter-v4.1.0
-mkdir "%DEPENDENCY_DIR%/czmq/build"
-cmake -H. -B"%DEPENDENCY_DIR%/czmq/build" -G "%GENERATOR%" -DCMAKE_INSTALL_PREFIX="%DEPENDENCY_DIR%/install"
-cmake --build "%DEPENDENCY_DIR%/czmq/build" --config %CONFIGURATION% --target INSTALL
+mkdir "%DEPENDENCY_DIR%\czmq\build"
+cmake -H. -B"%DEPENDENCY_DIR%\czmq\build" -G "%GENERATOR%" -DCMAKE_INSTALL_PREFIX="%DEPENDENCY_DIR%\install"
+cmake --build "%DEPENDENCY_DIR%\czmq\build" --config %CONFIGURATION% --target INSTALL
 popd
 
 REM Aquire patched hunterized msgpack
-IF NOT EXIST %DEPENDENCY_DIR%/msgpack-c git clone git clone https://github.com/mystfit/msgpack-c.git
+IF NOT EXIST %DEPENDENCY_DIR%\msgpack-c git clone https://github.com/mystfit/msgpack-c.git
 pushd msgpack-c
 git checkout hunter-2.1.5
-mkdir "%DEPENDENCY_DIR%/msgpack-c/build"
-cmake -H. -B"%DEPENDENCY_DIR%/msgpack-c/build" -G "%GENERATOR%" -DCMAKE_INSTALL_PREFIX="%DEPENDENCY_DIR%/install"
-cmake --build "%DEPENDENCY_DIR%/msgpack-c/build" --config %CONFIGURATION% --target INSTALL
+mkdir "%DEPENDENCY_DIR%\msgpack-c\build"
+cmake -H. -B"%DEPENDENCY_DIR%\msgpack-c\build" -G "%GENERATOR%" -DCMAKE_INSTALL_PREFIX="%DEPENDENCY_DIR%\install"
+cmake --build "%DEPENDENCY_DIR%\msgpack-c\build" --config %CONFIGURATION% --target INSTALL
 popd
 popd
 popd
--- test_local_appveyor.bat
+++ test_local_appveyor.bat
@@ -1,9 +1,10 @@
 ECHO ON
 
 REM Set up test Appveyor environment
-set APPVEYOR_BUILD_FOLDER=C:/projects/showtime-cpp
+set APPVEYOR_BUILD_FOLDER=C:\projects\showtime-cpp
 set BUILD_FOLDER=%APPVEYOR_BUILD_FOLDER%
-set DEPENDENCY_DIR=%BUILD_FOLDER%/dependencies
+set HUNTER_ROOT=C:\.hunter
+set DEPENDENCY_DIR=%BUILD_FOLDER%\dependencies
 set GENERATOR=Visual Studio 15 2017 Win64
 set CONFIGURATION=Debug
 
@@ -15,14 +16,14 @@ popd
 
 REM APPVEYOR install:
 REM ----------------------
-run "%APPVEYOR_BUILD_FOLDER%/install_dependencies.bat"
+"%APPVEYOR_BUILD_FOLDER%\install_dependencies.bat"
 
 REM APPVEYOR build_script:
 REM ----------------------
 pushd %APPVEYOR_BUILD_FOLDER%
 mkdir build
-cmake -H. -B"%APPVEYOR_BUILD_FOLDER%/build" -G "%GENERATOR%" -DCMAKE_PREFIX_PATH="%DEPENDENCY_DIR%/install;${CMAKE_PREFIX_PATH}"
-cmake --build "%APPVEYOR_BUILD_FOLDER%/build" --config %CONFIGURATION%
+cmake -H. -B"%APPVEYOR_BUILD_FOLDER%\build" -G "%GENERATOR%" -DCMAKE_PREFIX_PATH="%DEPENDENCY_DIR%\install;${CMAKE_PREFIX_PATH}"
+cmake --build "%APPVEYOR_BUILD_FOLDER%\build" --config %CONFIGURATION%
 popd
 
 REM APPVEYOR before_test:
@@ -30,10 +31,10 @@ REM ---------------------
 REM Copy CZMQ dlls into build folder - manually until CMake can do this automagically
 if %CONFIGURATION% == Debug set LIBCZMQ_DLL=libczmqd.dll
 if %CONFIGURATION% == Release set LIBCZMQ_DLL=libczmq.dll
-cmake -E copy "%DEPENDENCY_DIR%/install/bin/%LIBCZMQ_DLL%" "%APPVEYOR_BUILD_FOLDER%/build/bin/%CONFIGURATION%/"
+cmake -E copy "%DEPENDENCY_DIR%\install\bin\%LIBCZMQ_DLL%" "%APPVEYOR_BUILD_FOLDER%\build\bin\%CONFIGURATION%\"
 
 REM APPVEYOR test_script:
 REM ---------------------
-pushd "%APPVEYOR_BUILD_FOLDER%/build"
+pushd "%APPVEYOR_BUILD_FOLDER%\build"
 ctest -C Debug -V --output-on-fail
 popd
