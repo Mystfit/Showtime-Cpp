ab17dfa4aaa28d27e788fd3b4dd8fe455756eb71
Byron Mallett <byronated@gmail.com>
2018-03-02 19:00:05 +1300

    Added extra logs to install_linuxbrew.sh

--- ci/install_linuxbrew.sh
+++ ci/install_linuxbrew.sh
@@ -1,10 +1,11 @@
 if [ ! -d "$HOME/.linuxbrew" ]; then
-       	echo "Installing linuxbrew";
-	git clone https://github.com/Linuxbrew/brew.git ~/.linuxbrew;
+   	echo "Installing linuxbrew";
+	git clone https://github.com/Linuxbrew/brew.git $HOME/.linuxbrew;
+else
+	echo "Found existing linuxbrew directory"
 fi
-export HOMEBREW_FORCE_VENDOR_RUBY=1;
-PATH="$HOME/.linuxbrew/bin:$PATH";
-export MANPATH="$(brew --prefix)/share/man:$MANPATH";
-export INFOPATH="$(brew --prefix)/share/info:$INFOPATH";
+export HOMEBREW_FORCE_VENDOR_RUBY=1
+PATH="$HOME/.linuxbrew/bin:$PATH"
+export MANPATH="$(brew --prefix)/share/man:$MANPATH"
+export INFOPATH="$(brew --prefix)/share/info:$INFOPATH"
 echo "Set linuxbrew paths"
-
