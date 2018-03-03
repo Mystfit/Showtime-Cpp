fc47b7b637d0074ae9b003f12bd369136baa4b52
Byron Mallett <byronated@gmail.com>
2018-03-03 23:23:18 +1300

    Updated test time script path and fixed some blank dependency paths

--- install_dependencies.bat
+++ install_dependencies.bat
@@ -1,18 +1,31 @@
 ECHO ON
+setlocal
+
 REM ----------------------
 REM Environment variables
-IF NOT DEFINED BUILD_FOLDER set BUILD_FOLDER=C:\projects\showtime-cpp
+IF NOT DEFINED BUILD_FOLDER (
+	set BUILD_FOLDER=C:\projects\showtime-cpp
+)
 
 REM Set up dependency directories
 pushd %BUILD_FOLDER%
 
-IF NOT DEFINED DEPENDENCY_DIR set DEPENDENCY_DIR=%BUILD_FOLDER%\dependencies
+IF NOT DEFINED GENERATOR (
+	set GENERATOR=Visual Studio 15 2017 Win64
+)
+IF NOT DEFINED DEPENDENCY_DIR (
+	set DEPENDENCY_DIR=%BUILD_FOLDER%\dependencies
+)
 IF NOT EXIST %DEPENDENCY_DIR% (
     mkdir "%DEPENDENCY_DIR%"
     mkdir "%DEPENDENCY_DIR%\install"
 )
-IF NOT DEFINED HUNTER_ROOT set HUNTER_ROOT=%DEPENDENCY_DIR%\hunter_root
-IF NOT EXIST %HUNTER_ROOT% mkdir "%HUNTER_ROOT%"
+IF NOT DEFINED HUNTER_ROOT (
+	set HUNTER_ROOT=%DEPENDENCY_DIR%\hunter_root
+)
+IF NOT EXIST %HUNTER_ROOT% (
+	mkdir "%HUNTER_ROOT%"
+)
 
 REM Aquire patched hunterized CZMQ
 pushd "%DEPENDENCY_DIR%"
@@ -26,7 +39,9 @@ cmake --build "%DEPENDENCY_DIR%\czmq\build" --config %CONFIGURATION% --target IN
 popd
 
 REM Aquire patched hunterized msgpack
-IF NOT EXIST %DEPENDENCY_DIR%\msgpack-c git clone https://github.com/mystfit/msgpack-c.git
+IF NOT EXIST %DEPENDENCY_DIR%\msgpack-c (
+	git clone https://github.com/mystfit/msgpack-c.git
+)
 pushd msgpack-c
 git checkout hunter-2.1.5
 mkdir "%DEPENDENCY_DIR%\msgpack-c\build"
