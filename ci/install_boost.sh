fdfec2c4d983eab072214ae7b00306e0b33c3e08
Byron Mallett <byronated@gmail.com>
2018-03-04 03:47:22 +1300

    Changed boost flags around

--- ci/install_boost.sh
+++ ci/install_boost.sh
@@ -7,20 +7,14 @@
 # sudo apt-get install libboostchrono1.65-dev
 # sudo apt-get install libboostatomic1.65-dev
 
-B2_CMD=./b2 --prefix=$BOOST_ROOT link=static
-ALL_LIB_FLAGS=--with-thread --with-system --with-filesystem --with-log --with-date_time --with-chrono --with-atomic
+B2_CMD=./b2 --prefix=$BOOST_ROOT --toolset=gcc link=static 
+BOOST_LIBS=thread,system,filesystem,log,date_time,chrono,atomic
 
 wget https://dl.bintray.com/boostorg/release/1.66.0/source/boost_1_66_0.tar.bz2;
 tar --bzip2 -xf boost_1_66_0.tar.bz2;
 pushd boost_1_66_0;
 
 ./bootstrap.sh --prefix=$BOOST_ROOT
-$B2_CMD --with-thread stage
-$B2_CMD --with-system stage
-$B2_CMD --with-filesystem stage
-$B2_CMD --with-log stage
-$B2_CMD --with-date_time stage
-$B2_CMD --with-chrono stage
-$B2_CMD --with-atomic stage
-$B2_CMD $ALL_LIB_FLAGS install
+$B2_CMD --with-libraries=$BOOST_LIBS stage
+$B2_CMD --with-libraries=$BOOST_LIBS install
 popd
