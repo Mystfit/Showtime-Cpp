6fcef929bce62a8ed82d25522ac9479561f0f697
Byron Mallett <byronated@gmail.com>
2018-03-03 20:12:38 +1300

    Parsing ctest duration to appveyor test log

---
+++ ci/get_test_time.bat
@@ -0,0 +1,18 @@
+@echo off
+setlocal EnableDelayedExpansion
+
+rem Tail command in pure Batch, version 2: Tail.bat filename numOfLines
+rem Antonio Perez Ayala
+
+set /A firstTail=1, lastTail=0
+for /F "delims=" %%a in (%1) do (
+   set /A lastTail+=1, lines=lastTail-firstTail+1
+   set "lastLine[!lastTail!]=%%a"
+   if !lines! gtr 2 (
+      set "lastLine[!firstTail!]="
+      set /A firstTail+=1
+   )
+)
+for /L %%i in (%firstTail%,1,%lastTail%) do set lastline=!lastLine[%%i]!
+for /f "tokens=6" %%i in ("%lastline%") do set TEST_TIME=%%i
+for /f  "delims=" %%x in  ('powershell %TEST_TIME% * 1000') do echo %%x
