65d4e862b28f12f852b52a00b8269815ce67538a
Byron Mallett <byronated@gmail.com>
2018-03-05 21:05:54 +1300

    Attempting to fix boost lib flags again

--- ci/install_boost.sh
+++ ci/install_boost.sh
@@ -7,12 +7,18 @@
 # sudo apt-get install libboostchrono1.65-dev
 # sudo apt-get install libboostatomic1.65-dev
 
-BOOST_LIBS=thread,system,filesystem,log,date_time,chrono,atomic
+BOOST_FLAGS=--with-atomic
+BOOST_FLAGS=$BOOST_FLAGS --with-chrono
+BOOST_FLAGS=$BOOST_FLAGS --with-date_time
+BOOST_FLAGS=$BOOST_FLAGS --with-filesystem
+BOOST_FLAGS=$BOOST_FLAGS --with-log
+BOOST_FLAGS=$BOOST_FLAGS --with-system
+BOOST_FLAGS=$BOOST_FLAGS --with-thread
 
 wget https://dl.bintray.com/boostorg/release/1.66.0/source/boost_1_66_0.tar.bz2;
 tar --bzip2 -xf boost_1_66_0.tar.bz2;
 pushd boost_1_66_0;
 ./bootstrap.sh --prefix=$BOOST_ROOT
-./b2 --prefix=$BOOST_ROOT --toolset=gcc --with-libraries=thread,system,filesystem,log,date_time,chrono,atomic link=static stage
-./b2 --prefix=$BOOST_ROOT --toolset=gcc --with-libraries=thread,system,filesystem,log,date_time,chrono,atomic link=static install
+./b2 --prefix=$BOOST_ROOT --toolset=gcc $BOOST_FLAGS link=static stage
+./b2 --prefix=$BOOST_ROOT --toolset=gcc $BOOST_FLAGS link=static install
 popd
