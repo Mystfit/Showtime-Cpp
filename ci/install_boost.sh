11eef19e5e00a2a2b7975337042f3e3b3c8063d2
Byron Mallett <byronated@gmail.com>
2018-03-05 21:25:17 +1300

    Added quotes around boost flags

--- ci/install_boost.sh
+++ ci/install_boost.sh
@@ -7,13 +7,13 @@
 # sudo apt-get install libboostchrono1.65-dev
 # sudo apt-get install libboostatomic1.65-dev
 
-BOOST_FLAGS=--with-atomic
-BOOST_FLAGS=$BOOST_FLAGS --with-chrono
-BOOST_FLAGS=$BOOST_FLAGS --with-date_time
-BOOST_FLAGS=$BOOST_FLAGS --with-filesystem
-BOOST_FLAGS=$BOOST_FLAGS --with-log
-BOOST_FLAGS=$BOOST_FLAGS --with-system
-BOOST_FLAGS=$BOOST_FLAGS --with-thread
+BOOST_FLAGS="--with-atomic"
+BOOST_FLAGS="$BOOST_FLAGS --with-chrono"
+BOOST_FLAGS="$BOOST_FLAGS --with-date_time"
+BOOST_FLAGS="$BOOST_FLAGS --with-filesystem"
+BOOST_FLAGS="$BOOST_FLAGS --with-log"
+BOOST_FLAGS="$BOOST_FLAGS --with-system"
+BOOST_FLAGS="$BOOST_FLAGS --with-thread"
 
 wget https://dl.bintray.com/boostorg/release/1.66.0/source/boost_1_66_0.tar.bz2;
 tar --bzip2 -xf boost_1_66_0.tar.bz2;
