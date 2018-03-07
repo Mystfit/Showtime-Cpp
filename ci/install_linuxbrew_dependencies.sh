29f5f585d953e1a0d4d6d07b42bc0f8674016ddf
Byron Mallett <byronated@gmail.com>
2018-03-07 22:13:51 +1300

    Added linuxbrew vars

--- ci/install_linuxbrew_dependencies.sh
+++ ci/install_linuxbrew_dependencies.sh
@@ -5,7 +5,10 @@
 # export INFOPATH=$LINUXBREWHOME/share/info:$INFOPATH
 # export PKG_CONFIG_PATH=$LINUXBREWHOME/lib64/pkgconfig:$LINUXBREWHOME/lib/pkgconfig:$PKG_CONFIG_PATH
 # export LD_LIBRARY_PATH=$LINUXBREWHOME/lib64:$LINUXBREWHOME/lib:$LD_LIBRARY_PATH
-
+echo 'export PATH="/home/linuxbrew/.linuxbrew/bin:$PATH"' >>~/.profile
+echo 'export MANPATH="/home/linuxbrew/.linuxbrew/share/man:$MANPATH"' >>~/.profile
+echo 'export INFOPATH="/home/linuxbrew/.linuxbrew/share/info:$INFOPATH"' >>~/.profile
+PATH="/home/linuxbrew/.linuxbrew/bin:$PATH"
 brew install msgpack
 brew install fmt
 brew install boost
