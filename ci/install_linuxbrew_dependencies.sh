9ccb0262c2fa96a7f500dec0009704a35ce7dee4
Byron Mallett <byronated@gmail.com>
2018-03-07 21:52:33 +1300

    Attempting to install linuxbrew as sudo

--- ci/install_linuxbrew_dependencies.sh
+++ ci/install_linuxbrew_dependencies.sh
@@ -1,10 +1,10 @@
-git clone https://github.com/Linuxbrew/brew.git $LINUXBREWHOME;
-export HOMEBREW_FORCE_VENDOR_RUBY=1
-export PATH=$LINUXBREWHOME/bin:$PATH
-export MANPATH=$LINUXBREWHOME/share/man:$MANPATH
-export INFOPATH=$LINUXBREWHOME/share/info:$INFOPATH
-export PKG_CONFIG_PATH=$LINUXBREWHOME/lib64/pkgconfig:$LINUXBREWHOME/lib/pkgconfig:$PKG_CONFIG_PATH
-export LD_LIBRARY_PATH=$LINUXBREWHOME/lib64:$LINUXBREWHOME/lib:$LD_LIBRARY_PATH
+# git clone https://github.com/Linuxbrew/brew.git $LINUXBREWHOME;
+# export HOMEBREW_FORCE_VENDOR_RUBY=1
+# export PATH=$LINUXBREWHOME/bin:$PATH
+# export MANPATH=$LINUXBREWHOME/share/man:$MANPATH
+# export INFOPATH=$LINUXBREWHOME/share/info:$INFOPATH
+# export PKG_CONFIG_PATH=$LINUXBREWHOME/lib64/pkgconfig:$LINUXBREWHOME/lib/pkgconfig:$PKG_CONFIG_PATH
+# export LD_LIBRARY_PATH=$LINUXBREWHOME/lib64:$LINUXBREWHOME/lib:$LD_LIBRARY_PATH
 
 brew install msgpack
 brew install fmt
