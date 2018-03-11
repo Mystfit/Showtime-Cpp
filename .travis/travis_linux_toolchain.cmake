421d968795df12347c2fb8ebe114b4f669bc105d
Byron Mallett <byronated@gmail.com>
2018-03-11 19:04:46 +1300

    Setting homebrew linker in toolchain file

--- .travis/travis_linux_toolchain.cmake
+++ .travis/travis_linux_toolchain.cmake
@@ -8,6 +8,11 @@ SET(CMAKE_SYSTEM_NAME Linux)
 # specify the cross compiler
 set(CMAKE_C_COMPILER /home/linuxbrew/.linuxbrew/bin/gcc CACHE STRING "" FORCE)
 set(CMAKE_CXX_COMPILER /home/linuxbrew/.linuxbrew/bin/g++ CACHE STRING "" FORCE)
+set(CMAKE_LINKER /home/linuxbrew/.linuxbrew/bin/ld CACHE STRING "" FORCE)
+set(LINKER_RULE "<CMAKE_LINKER> <FLAGS> <CMAKE_CXX_LINK_FLAGS> <LINK_FLAGS> <OBJECTS> -o <TARGET> <LINK_LIBRARIES>")
+set(CMAKE_CXX_LINK_EXECUTABLE ${LINKER_RULE})
+set(CMAKE_CXX_LINK_SHARED ${LINKER_RULE})
+set(CMAKE_CXX_LINK_MODULE ${LINKER_RULE})
 
 # Set these vars as active to skip the compiler check
 # SET(CMAKE_C_COMPILER_WORKS 1 CACHE INTERNAL "") 
