dcfb72c8f74700182402ab46202acd9b317bd749
Byron Mallett <byronated@gmail.com>
2018-03-11 18:23:52 +1300

    Fixing toolchain parse error

--- .travis/travis_linux_toolchain.cmake
+++ .travis/travis_linux_toolchain.cmake
@@ -6,8 +6,8 @@ SET(CMAKE_SYSTEM_NAME Linux)
 #SET(CMAKE_SYSTEM_VERSION 1)
 
 # specify the cross compiler
-set CMAKE_C_COMPILER(/home/linuxbrew/.linuxbrew/bin/gcc CACHE STRING "" FORCE)
-set CMAKE_CXX_COMPILER(/home/linuxbrew/.linuxbrew/bin/g++ CACHE STRING "" FORCE)
+set(CMAKE_C_COMPILER /home/linuxbrew/.linuxbrew/bin/gcc CACHE STRING "" FORCE)
+set(CMAKE_CXX_COMPILER /home/linuxbrew/.linuxbrew/bin/g++ CACHE STRING "" FORCE)
 
 # Set these vars as active to skip the compiler check
 # SET(CMAKE_C_COMPILER_WORKS 1 CACHE INTERNAL "") 
