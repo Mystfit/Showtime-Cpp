9e42fa768d7fc925dfffafe9bd4e6678e03355cd
Byron Mallett <byronated@gmail.com>
2018-03-22 22:22:21 +1300

    Added some more debugging messages in regards to hunter. Readded zeromq as a hunter package to try and solve missing Czmq errors

--- .appveyor/test_local_appveyor.bat
+++ .appveyor/test_local_appveyor.bat
@@ -1,15 +1,16 @@
-@echo off
+echo off
+setlocal
 
 REM Set up test Appveyor environment
 set APPVEYOR_BUILD_FOLDER=C:\projects\showtime-cpp
-REM set HUNTER_ROOT=C:\.hunter
+set HUNTER_ROOT=%APPVEYOR_BUILD_FOLDER%\dependencies\hunter_root
 set GENERATOR=Visual Studio 15 2017 Win64
 set CONFIGURATION=Debug
 
 REM Clone showtime repository
-git clone https://github.com/mystfit/showtime-cpp.git %APPVEYOR_BUILD_FOLDER%
+REM git clone https://github.com/mystfit/showtime-cpp.git %APPVEYOR_BUILD_FOLDER%
 pushd %APPVEYOR_BUILD_FOLDER%
-git checkout develop
+REM git checkout appveyor
 popd
 
 REM APPVEYOR install:
@@ -17,14 +18,25 @@ REM ----------------------
 set BUILD_FOLDER=%APPVEYOR_BUILD_FOLDER%
 set DEPENDENCY_DIR=%BUILD_FOLDER%\dependencies
 call "%APPVEYOR_BUILD_FOLDER%\.appveyor\install_dependencies.bat"
+set CMAKE_FLAGS=-DCMAKE_PREFIX_PATH="%DEPENDENCY_DIR%\install;%DEPENDENCY_DIR%\swig;${CMAKE_PREFIX_PATH}"
+set PYTHON=C:\Program Files (x86)\Microsoft Visual Studio\Shared\Python36_64
 
 REM APPVEYOR build_script:
 REM ----------------------
 pushd %APPVEYOR_BUILD_FOLDER%
 mkdir build
-set CMAKE_FLAGS=-DCMAKE_PREFIX_PATH="%DEPENDENCY_DIR%\install;${CMAKE_PREFIX_PATH}"
-cmake -H. -B"%APPVEYOR_BUILD_FOLDER%\build" -G "%GENERATOR%" %CMAKE_FLAGS%
-cmake --build "%APPVEYOR_BUILD_FOLDER%\build" --config %CONFIGURATION%
+set CMAKE_FLAGS=%CMAKE_FLAGS% -DBINDINGS_DOTNET=ON -DBINDINGS_DOTNET_TESTS=ON -DBINDINGS_PYTHON=ON -DBINDINGS_PYTHON_TESTS=ON -DPYTHON_EXECUTABLE="%PYTHON%\python.exe"
+    
+REM Build core and dotnet bindings first
+cmake -H. -B"%BUILD_FOLDER%\build" -G "%GENERATOR%" %CMAKE_FLAGS%
+cmake --build "%BUILD_FOLDER%\build" --target ShowtimeDotNet --config %CONFIGURATION% -- /nologo /verbosity:minimal
+
+REM Re-run configure to pull in the generated dotnet bindings
+cmake -H. -B"%BUILD_FOLDER%\build" -G "%GENERATOR%" %CMAKE_FLAGS%
+cmake --build "%BUILD_FOLDER%\build" --target ALL_BUILD --config %CONFIGURATION% -- /nologo /verbosity:minimal
+
+REM Install showtime
+cmake --build "%BUILD_FOLDER%\build" --target INSTALL --config %CONFIGURATION% -- /nologo /verbosity:minimal
 popd
 
 REM APPVEYOR before_test:
