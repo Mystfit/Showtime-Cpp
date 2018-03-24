9000464abff5e63e72e844b5f8cfba2466dcc884
Byron Mallett <byronated@gmail.com>
2018-03-24 14:59:53 +1300

    Removed quotes around dependency script call

--- .appveyor/test_local_appveyor.bat
+++ .appveyor/test_local_appveyor.bat
@@ -7,50 +7,44 @@ set HUNTER_ROOT=%APPVEYOR_BUILD_FOLDER%\dependencies\hunter_root
 set GENERATOR=Visual Studio 15 2017 Win64
 set CONFIGURATION=Debug
 
-REM Clone showtime repository
-REM git clone https://github.com/mystfit/showtime-cpp.git %APPVEYOR_BUILD_FOLDER%
-pushd %APPVEYOR_BUILD_FOLDER%
-REM git checkout appveyor
-popd
-
 REM APPVEYOR install:
 REM ----------------------
 set BUILD_FOLDER=%APPVEYOR_BUILD_FOLDER%
 set DEPENDENCY_DIR=%BUILD_FOLDER%\dependencies
-call "%APPVEYOR_BUILD_FOLDER%\.appveyor\install_dependencies.bat"
+call %BUILD_FOLDER%/.appveyor/install_dependencies.bat %BUILD_FOLDER% %CONFIGURATION%
 set CMAKE_FLAGS=-DCMAKE_PREFIX_PATH="%DEPENDENCY_DIR%\install;%DEPENDENCY_DIR%\swig;${CMAKE_PREFIX_PATH}"
 set PYTHON=C:\Program Files (x86)\Microsoft Visual Studio\Shared\Python36_64
 
 REM APPVEYOR build_script:
 REM ----------------------
-pushd %APPVEYOR_BUILD_FOLDER%
-mkdir build
-set CMAKE_FLAGS=%CMAKE_FLAGS% -DBINDINGS_DOTNET=ON -DBINDINGS_DOTNET_TESTS=ON -DBINDINGS_PYTHON=ON -DBINDINGS_PYTHON_TESTS=ON -DPYTHON_EXECUTABLE="%PYTHON%\python.exe"
-    
+mkdir %BUILD_FOLDER%\build
+set CMAKE_FLAGS=%CMAKE_FLAGS% -DBINDINGS_DOTNET=ON -DBINDINGS_DOTNET_TESTS=ON
+set CMAKE_FLAGS=%CMAKE_FLAGS% -DBINDINGS_PYTHON=ON -DBINDINGS_PYTHON_TESTS=ON -DPYTHON_EXECUTABLE=%PYTHON%/python.exe
+set CMAKE_FLAGS=%CMAKE_FLAGS% -DHUNTER_STATUS_PRINT=OFF
+set CMAKE_FLAGS=%CMAKE_FLAGS% -DCMAKE_INSTALL_PREFIX=%INSTALL_DIR%
+
 REM Build core and dotnet bindings first
-cmake -H. -B"%BUILD_FOLDER%\build" -G "%GENERATOR%" %CMAKE_FLAGS%
+cmake -H"%BUILD_FOLDER%" -B"%BUILD_FOLDER%\build" -G "%GENERATOR%" %CMAKE_FLAGS%
 cmake --build "%BUILD_FOLDER%\build" --target ShowtimeDotNet --config %CONFIGURATION% -- /nologo /verbosity:minimal
 
 REM Re-run configure to pull in the generated dotnet bindings
-cmake -H. -B"%BUILD_FOLDER%\build" -G "%GENERATOR%" %CMAKE_FLAGS%
+cmake -H"%BUILD_FOLDER%" -B"%BUILD_FOLDER%\build" -G "%GENERATOR%" %CMAKE_FLAGS%
 cmake --build "%BUILD_FOLDER%\build" --target ALL_BUILD --config %CONFIGURATION% -- /nologo /verbosity:minimal
 
 REM Install showtime
 cmake --build "%BUILD_FOLDER%\build" --target INSTALL --config %CONFIGURATION% -- /nologo /verbosity:minimal
-popd
 
 REM APPVEYOR before_test:
 REM ---------------------
 REM Copy CZMQ dlls into build folder - manually until CMake can do this automagically
 if %CONFIGURATION% == Debug set LIBCZMQ_RUNTIME=libczmqd.dll
 if %CONFIGURATION% == Release set LIBCZMQ_RUNTIME=libczmq.dll
-cmake -E copy "%DEPENDENCY_DIR%\install\bin\%LIBCZMQ_RUNTIME%" "%APPVEYOR_BUILD_FOLDER%\build\bin\%CONFIGURATION%"
+cmake -E copy "%DEPENDENCY_DIR%\install\bin\%LIBCZMQ_RUNTIME%" "%BUILD_FOLDER%\build\bin\%CONFIGURATION%"
 
 REM APPVEYOR test_script:
 REM ---------------------
-pushd "%BUILD_FOLDER%\build"
 set TESTCLIENT_LOG=%BUILD_FOLDER%\build\Testing\TestClient.log
-ctest -C %CONFIGURATION% --output-on-fail -V -O "%TESTCLIENT_LOG%" --timeout 120
+ctest --build-run-dir "%BUILD_FOLDER%\build" -C %CONFIGURATION% --output-on-fail -V -O "%TESTCLIENT_LOG%" --timeout 120
 set TEST_OUTCOME=Passed
 if NOT %errorlevel% == 0 set TEST_OUTCOME=Failed
 call "%BUILD_FOLDER%\ci\get_test_time.bat" %TESTCLIENT_LOG% TEST_DURATION
