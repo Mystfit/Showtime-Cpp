36448aba0b2667bf3c718402f247309a9cb2ebcd
Byron Mallett <byronated@gmail.com>
2018-08-01 19:38:28 +1200

    Added windows signal handling for server. Replaced custom version var with cmake project version var

--- src/server/ZstStageRunner.cpp
+++ src/server/ZstStageRunner.cpp
@@ -1,7 +1,12 @@
 #include "ZstStage.h"
 #include "ZstVersion.h"
 #include <stdio.h>
+
+#ifdef WIN32
+#include <windows.h>
+#else
 #include <signal.h>
+#endif
 
 //Standalone stage runner
 //-----------------------
@@ -13,42 +18,64 @@
 #endif
 
 static int s_interrupted = 0;
-static void s_signal_handler(int signal_value)
+
+static bool s_signal_handler(unsigned long signal_value)
 {
-	ZstLog::app(LogLevel::notification, "Caught signal {}", signal_value);
-	s_interrupted = 1;
+	ZstLog::app(LogLevel::debug, "Caught signal {}", signal_value);
+#ifdef WIN32
+	switch (signal_value) {
+	case CTRL_C_EVENT:
+		s_interrupted = 1;
+		return true;
+	case CTRL_CLOSE_EVENT:
+		s_interrupted = 1;
+		return true;
+#else
+	case SIGINT:
+		s_interrupted = 1;
+		return true;
+	case SIGTERM:
+		s_interrupted = 1;
+		return true;
+	case SIGKILL:
+		s_interrupted = 1;
+		return true;
+	case SIGABRT:
+		s_interrupted = 1;
+		return true;
+#endif
+	default:
+		break;
+	}
+	return false;
 }
 
 static void s_catch_signals(){
+#ifdef WIN32
+	if (!SetConsoleCtrlHandler((PHANDLER_ROUTINE)s_signal_handler, TRUE)) {
+		ZstLog::app(LogLevel::error, "Unable to register Control Handler");
+	}
+#else
 	struct sigaction action;
 	action.sa_handler = s_signal_handler;
 	action.sa_flags = 0;
 	sigemptyset(&action.sa_mask);
 	sigaction(SIGINT, &action, NULL);
 	sigaction(SIGTERM, &action, NULL);
+#endif
 }
 
-int main(int argc, char **argv) {
-
-#ifdef WIN32
-	Sleep(100);
-#else
-    usleep(1000 * 200);
-#endif
-
+int main(int argc, char **argv)
+{
 	ZstStage stage;
 	stage.init_stage("stage", true);
 
 	if (argc < 2) {
 		ZstLog::app(LogLevel::notification, "Stage running in standalone mode. Press Ctrl+C to exit");
-#ifdef WIN32
-		system("pause");
-#else
 		s_catch_signals();
 		while(!s_interrupted){
 			TAKE_A_BREATH
 		}
-#endif
 	}
 	else {
 		if (strcmp(argv[1], "-t") == 0)
@@ -59,12 +86,11 @@ int main(int argc, char **argv) {
 				std::getline(std::cin, line);
 				TAKE_A_BREATH
 			} while (line != "$TERM");
-				ZstLog::app(LogLevel::notification, "Received $TERM. Closing stage server.");
-			}
+			ZstLog::app(LogLevel::notification, "Received $TERM. Closing stage server.");
+		}
 	}
 
 	std::cout << "Showtime Stage shutting down" << std::endl;
-
 	stage.destroy();
 
 	return 0;
