e1605b38c9487c2bc88189857ae608c8983fd433
Byron Mallett <byronated@gmail.com>
2018-10-17 16:50:52 +1300

    Started plugin interface. Added RTmidi plugin as a template

--- include/ZstPlugin.h
+++ include/ZstPlugin.h
@@ -2,6 +2,18 @@
 
 #include <ZstCore.h>
 
-extern "C" {
-    
-}
+#include <boost/config.hpp>
+#include <memory>
+
+// A plugin is a collection of factories and entities that are loaded at runtime
+
+class ZstPlugin {
+public:
+	virtual void init(const char * root_name) = 0;
+	virtual const char * name() = 0;
+	virtual int version_major() = 0;
+	virtual int version_minor() = 0;
+	virtual int version_patch() = 0;
+
+	virtual void get_factories(ZstEntityBundle & bundle) = 0;
+};
