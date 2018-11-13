6d317346e3193d4829130f55b524c7e4ceb35427
Byron Mallett <byronated@gmail.com>
2018-11-14 00:48:19 +1300

    Fixing var symbols in appveyor test block

--- .appveyor/test_loop.bat
+++ .appveyor/test_loop.bat
@@ -1,16 +1,5 @@
-SETLOCAL EnableDelayedExpansion
+set BUILD_FOLDER=%cd%
 
-set tests[0]="TestStartup"
-set tests[1]="TestEntities"
-set tests[2]="TestGraph"
-set tests[3]="TestExternalClients"
-set tests[4]="TestPlugins"
-set tests[5]="TestEntityFactories"
-set tests[6]="TestBenchmark"
-
-for /L %%i in (0,1,6) do (
-    set test=!tests[%%i]!
-    echo !test!
+for /F "Tokens=* Delims=" %%A in (%BUILD_FOLDER%\..\.appveyor\tests.txt) do (
+    echo Adding %%A test
 )
-
-ENDLOCAL
