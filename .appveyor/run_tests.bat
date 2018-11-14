dd366d446305ab21269b0e30092ac5991b3356da
Byron Mallett <byronated@gmail.com>
2018-11-15 12:40:40 +1300

    Moved tests back into appveyor yml

--- .appveyor/run_tests.bat
+++ .appveyor/run_tests.bat
@@ -4,20 +4,15 @@ appveyor AddMessage "Running tests from %BUILD_FOLDER%\.appveyor\tests.txt"
 pushd %BUILD_FOLDER%\build
 
 for /F "Tokens=* Delims=" %%A in (%BUILD_FOLDER%\.appveyor\tests.txt) do (
-	echo "Running appveyor AddTest %%A -Framework ctest -Filename %%A.exe -Outcome Running"
     appveyor AddTest %%A -Framework ctest -Filename %%A.exe -Outcome Running
     set TEST_LOG=%BUILD_FOLDER%\build\Testing\%%A.log
-
-    echo "Running %CTEST_BIN% -R %%A -C %CONFIGURATION% --output-on-fail -V -O %TEST_LOG% --timeout 320"
     %CTEST_BIN% -R %%A -C %CONFIGURATION% --output-on-fail -V -O %TEST_LOG% --timeout 320
 
     set TEST_OUTCOME=Passed
     if NOT %errorlevel% == 0 set TEST_OUTCOME=Failed
+    call "%BUILD_FOLDER%/.appveyor/get_test_time.bat" %TEST_LOG% TEST_DURATION
 
-    echo "Running appveyor UpdateTest -Name %%A -Framework ctest -Filename %%A.exe -Outcome !TEST_OUTCOME!"
-    appveyor UpdateTest -Name %%A -Framework ctest -Filename %%A.exe -Outcome !TEST_OUTCOME!
-)
-
+    appveyor UpdateTest -Name %%A -Framework ctest -Filename %%A.exe -Outcome !TEST_OUTCOME! -Duration !TEST_DURATION!
 popd
 
 ENDLOCAL
