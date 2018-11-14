af97c24ca58a847d484b393c6c40f37a00630d37
Byron Mallett <byronated@gmail.com>
2018-11-14 14:35:05 +1300

    Added appveyor remote desktop. Flipped test directory slash directory

--- .appveyor/run_tests.bat
+++ .appveyor/run_tests.bat
@@ -1,12 +1,12 @@
 SETLOCAL EnableDelayedExpansion
-appveyor AddMessage "Running tests from %BUILD_FOLDER%/.appveyor/tests.txt"
+appveyor AddMessage "Running tests from %BUILD_FOLDER%\.appveyor\tests.txt"
 
-pushd %BUILD_FOLDER%/build
+pushd %BUILD_FOLDER%\build
 
 for /F "Tokens=* Delims=" %%A in (%BUILD_FOLDER%\.appveyor\tests.txt) do (
 	echo "Running appveyor AddTest %%A -Framework ctest -Filename %%A.exe -Outcome Running"
     appveyor AddTest %%A -Framework ctest -Filename %%A.exe -Outcome Running
-    set TEST_LOG=%BUILD_FOLDER%/build/Testing/%%A.log
+    set TEST_LOG=%BUILD_FOLDER%\build\Testing\%%A.log
 
     echo "Running %CTEST_BIN% -R %%A -C %CONFIGURATION% --output-on-fail -V -O %TEST_LOG% --timeout 320"
     %CTEST_BIN% -R %%A -C %CONFIGURATION% --output-on-fail -V -O %TEST_LOG% --timeout 320
