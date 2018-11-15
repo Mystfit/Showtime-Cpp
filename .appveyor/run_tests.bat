4c30a1aaf9a63f0b6094d6e557ab453c8dfbca18
Byron Mallett <byronated@gmail.com>
2018-11-15 14:21:24 +1300

    Some slash reversals and creating some missing directories

--- .appveyor/run_tests.bat
+++ .appveyor/run_tests.bat
@@ -1,8 +1,9 @@
 SETLOCAL EnableDelayedExpansion
 appveyor AddMessage "Running tests from %BUILD_FOLDER%\.appveyor\tests.txt"
 
-pushd %BUILD_FOLDER%\build
+mkdir %BUILD_FOLDER%\build\Testing
 
+pushd %BUILD_FOLDER%\build
 for /F "Tokens=* Delims=" %%A in (%BUILD_FOLDER%\.appveyor\tests.txt) do (
     appveyor AddTest %%A -Framework ctest -Filename %%A.exe -Outcome Running
     set TEST_LOG=%BUILD_FOLDER%\build\Testing\%%A.log
@@ -10,7 +11,7 @@ for /F "Tokens=* Delims=" %%A in (%BUILD_FOLDER%\.appveyor\tests.txt) do (
 
     set TEST_OUTCOME=Passed
     if NOT %errorlevel% == 0 set TEST_OUTCOME=Failed
-    call "%BUILD_FOLDER%/.appveyor/get_test_time.bat" %TEST_LOG% TEST_DURATION
+    call "%BUILD_FOLDER%\.appveyor\get_test_time.bat" %TEST_LOG% TEST_DURATION
 
     appveyor UpdateTest -Name %%A -Framework ctest -Filename %%A.exe -Outcome !TEST_OUTCOME! -Duration !TEST_DURATION!
 popd
