c7c563005d0f182af84e5cb195546879de10ec4d
Byron Mallett <byronated@gmail.com>
2018-11-26 21:08:39 +1300

    Now compiling python c extension via cmake rather than distutils

--- src/bindings/python/generate-manifest.cmake
+++ src/bindings/python/generate-manifest.cmake
@@ -5,9 +5,5 @@ if (NOT SUBST_DEST)
     message(FATAL_ERROR "Must set -D SUBST_DEST=dest_file")
 endif()
 
-# Find boost shared runtimes (windows only)
-file(GLOB BOOST_RUNTIMES ${BOOST_DLL_PATTERN})
-list(APPEND ZST_RUNTIMES ${BOOST_RUNTIMES})
-
 message(STATUS "Formatting setup.py - ${SUBST_SOURCE}  ${SUBST_DEST}")
 configure_file("${SUBST_SOURCE}" "${SUBST_DEST}" @ONLY)
