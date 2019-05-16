8faaee16eb26662d7ab5fccddee6d6c365c9471c
Byron Mallett <byronated@gmail.com>
2019-05-17 01:55:14 +1200

    Dependency installer now builds android deps
    Also removed swig and cmake downloads. These need to be on the path or specified manually

--- install_dependencies.bat
+++ install_dependencies.bat
@@ -8,13 +8,26 @@ SET ARG=%1
 IF DEFINED ARG (
     IF "%ARG%" EQU "--build-dir" (
         set BUILD_FOLDER=%2
-       
+        SHIFT
+    )
+
+    IF "%ARG%" EQU "--platform" (
+        set PLATFORM=%2
+        SHIFT
+    )
+
+    IF "%ARG%" EQU "--toolchain" (
+        set TOOLCHAIN_CMAKE=%2
+        SHIFT
+    )
+
+    IF "%ARG%" EQU "--generator" (
+        set GENERATOR=%2
         SHIFT
     )
 
     IF "%ARG%" EQU "--config" (
         set CONFIGURATION=%2
-        
         SHIFT
     )
 
@@ -23,18 +36,16 @@ IF DEFINED ARG (
         echo With Boost
     )
 
-    IF "%ARG%" EQU "--with-unity" (
-        set WITH_UNITY=1
-        echo With Unity
-    )
-
     SHIFT
     GOTO processargs
 )
 
 REM Expand variables supplied from :processargs
 set BUILD_FOLDER=!BUILD_FOLDER!
+set PLATFORM=!PLATFORM!
 set CONFIGURATION=!CONFIGURATION!
+set GENERATOR=!GENERATOR!
+set TOOLCHAIN_CMAKE=!TOOLCHAIN_CMAKE!
 
 REM Default paths and variables
 IF NOT DEFINED BUILD_FOLDER (
@@ -45,6 +56,7 @@ echo Build folder=!BUILD_FOLDER!
 IF NOT DEFINED CONFIGURATION (
     set CONFIGURATION=debug
 )
+
 echo Configuration=!CONFIGURATION!
 for /F %%s IN ('python -c "print(\"%CONFIGURATION%\".lower())"') DO set CONFIG_LOWER=%%s
 
@@ -56,40 +68,52 @@ IF NOT DEFINED WITH_BOOST (
     set WITH_BOOST=0
 )
 
-set GENERATOR=Visual Studio 15 2017 Win64
+IF NOT DEFINED PLATFORM (
+    set PLATFORM=win64
+)
+echo Platform=!PLATFORM!
+
+IF NOT DEFINED TOOLCHAIN_CMAKE (
+    set TOOLCHAIN_CMAKE=0
+)
+
+IF NOT DEFINED GENERATOR (
+    IF %PLATFORM% EQU android (
+        set GENERATOR="Ninja"
+    ) ELSE (
+        set GENERATOR="Visual Studio 15 2017 Win64"
+    )
+)
+
 set DEPENDENCY_DIR=%BUILD_FOLDER%\dependencies
 IF NOT EXIST %DEPENDENCY_DIR% (
     mkdir "%DEPENDENCY_DIR%"
     mkdir "%DEPENDENCY_DIR%\install"
 )
 
-REM CMake
-set CMAKE_VER=3.13.0
-set CMAKE_VER_FULL=cmake-%CMAKE_VER%-win64-x64
-set CMAKE_URL=https://cmake.org/files/v3.13/%CMAKE_VER_FULL%.zip
-IF EXIST %DEPENDENCY_DIR%\cmake (
-    echo Found CMake
-) ELSE (
-    echo === Downloading %CMAKE_VER_FULL% === 
-    powershell -Command "[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; Invoke-WebRequest %CMAKE_URL% -OutFile %DEPENDENCY_DIR%\cmake.zip"
-    
-    echo  === Unzipping CMake === 
-    7z x -y -bd -bb0 -o%DEPENDENCY_DIR% %DEPENDENCY_DIR%\cmake.zip
-    echo Renaming %DEPENDENCY_DIR%\%CMAKE_VER_FULL% to cmake
-    rename "%DEPENDENCY_DIR%\%CMAKE_VER_FULL%" cmake
-)
-
-set CMAKE_BIN=%DEPENDENCY_DIR%\cmake\bin\cmake
-set CTEST_BIN=%DEPENDENCY_DIR%\cmake\bin\ctest
 
 REM Set common build flags and prefixes for czmq and msgpack  
 set INSTALL_PREFIX=%DEPENDENCY_DIR%\install
-set COMMON_GENERATOR_FLAGS=-G "%GENERATOR%" -DCMAKE_INSTALL_PREFIX="%INSTALL_PREFIX%" -DCMAKE_INSTALL_MESSAGE=NEVER -DCMAKE_PREFIX_PATH="%INSTALL_PREFIX%"
-set COMMON_BUILD_FLAGS=--config %CONFIGURATION% --target INSTALL -- /nologo /verbosity:minimal
+set COMMON_GENERATOR_FLAGS=-DCMAKE_INSTALL_MESSAGE=NEVER -DCMAKE_INSTALL_PREFIX="%INSTALL_PREFIX%" -DCMAKE_PREFIX_PATH="%INSTALL_PREFIX%"
+set COMMON_BUILD_FLAGS=--config %CONFIGURATION% --target install
+
+IF DEFINED GENERATOR (
+    set COMMON_GENERATOR_FLAGS=!COMMON_GENERATOR_FLAGS! -G !GENERATOR!
+)
+
+IF %PLATFORM% EQU android (
+    echo %TOOLCHAIN_CMAKE%
+    IF !TOOLCHAIN_CMAKE! EQU 0 (
+        echo Platform set to android but no toolchain cmake file was provided with the --toolchain flag
+        exit /B 1
+    )
+    REM C:\Users\mystfit\AppData\Local\Android\Sdk\ndk-bundle\build\cmake\android.toolchain.cmake
+    set COMMON_GENERATOR_FLAGS=!COMMON_GENERATOR_FLAGS! -DCMAKE_TOOLCHAIN_FILE=%TOOLCHAIN_CMAKE% -DANDROID_PLATFORM=28
+)
 
 
 REM libZMQ
-IF EXIST %DEPENDENCY_DIR%\libzmq\build (
+IF EXIST %DEPENDENCY_DIR%\install\include\zmq.h (
     echo Found libZMQ
 ) ELSE (
     echo === Cloning libZMQ === 
@@ -100,12 +124,11 @@ IF EXIST %DEPENDENCY_DIR%\libzmq\build (
     mkdir "%DEPENDENCY_DIR%\libzmq\build"
 )
 echo  === Building libzmq === 
-%CMAKE_BIN% -H"%DEPENDENCY_DIR%\libzmq" -B"%DEPENDENCY_DIR%\libzmq\build" %COMMON_GENERATOR_FLAGS% -DENABLE_DRAFTS=TRUE -DZMQ_BUILD_TESTS=OFF
-%CMAKE_BIN% --build "%DEPENDENCY_DIR%\libzmq\build" %COMMON_BUILD_FLAGS%
-
+cmake -H"%DEPENDENCY_DIR%\libzmq" -B"%DEPENDENCY_DIR%\libzmq\build" %COMMON_GENERATOR_FLAGS% -DENABLE_DRAFTS=TRUE -DZMQ_BUILD_TESTS=OFF
+cmake --build "%DEPENDENCY_DIR%\libzmq\build" %COMMON_BUILD_FLAGS%
 
 REM CZMQ
-IF EXIST %DEPENDENCY_DIR%\czmq\build (
+IF EXIST %DEPENDENCY_DIR%\install\include\czmq.h (
     echo Found CZMQ
 ) ELSE (
     echo === Cloning CZMQ === 
@@ -114,12 +137,12 @@ IF EXIST %DEPENDENCY_DIR%\czmq\build (
     mkdir "%DEPENDENCY_DIR%\czmq\build"
 )
 echo  === Building czmq === 
-%CMAKE_BIN% -H"%DEPENDENCY_DIR%\czmq" -B"%DEPENDENCY_DIR%\czmq\build" %COMMON_GENERATOR_FLAGS% -DENABLE_DRAFTS=TRUE -DBUILD_TESTING=OFF -DLIBZMQ_FIND_USING_CMAKE_PACKAGE=ON
-%CMAKE_BIN% --build "%DEPENDENCY_DIR%\czmq\build" %COMMON_BUILD_FLAGS%
+cmake -H"%DEPENDENCY_DIR%\czmq" -B"%DEPENDENCY_DIR%\czmq\build" %COMMON_GENERATOR_FLAGS% -DENABLE_DRAFTS=TRUE -DBUILD_TESTING=OFF -DLIBZMQ_FIND_USING_CMAKE_PACKAGE=ON -DZeroMQ_DIR=%INSTALL_PREFIX%\share\cmake\ZeroMQ
+cmake --build "%DEPENDENCY_DIR%\czmq\build" %COMMON_BUILD_FLAGS%
 
 
 REM msgpack-c
-IF EXIST %DEPENDENCY_DIR%\msgpack-c\build (
+IF EXIST %DEPENDENCY_DIR%\install\include\msgpack.hpp (
     echo Found MsgPack-C
 ) ELSE (
     echo === Cloning msgpack-c === 
@@ -129,8 +152,8 @@ IF EXIST %DEPENDENCY_DIR%\msgpack-c\build (
     mkdir "%DEPENDENCY_DIR%\msgpack-c\build"
 )
 echo === Building msgpack === 
-%CMAKE_BIN% -H"%DEPENDENCY_DIR%\msgpack-c" -B"%DEPENDENCY_DIR%\msgpack-c\build" %COMMON_GENERATOR_FLAGS% -DMSGPACK_BUILD_EXAMPLES=OFF
-%CMAKE_BIN% --build "%DEPENDENCY_DIR%\msgpack-c\build" %COMMON_BUILD_FLAGS%
+cmake -H"%DEPENDENCY_DIR%\msgpack-c" -B"%DEPENDENCY_DIR%\msgpack-c\build" %COMMON_GENERATOR_FLAGS% -DMSGPACK_BUILD_EXAMPLES=OFF
+cmake --build "%DEPENDENCY_DIR%\msgpack-c\build" %COMMON_BUILD_FLAGS%
 
 
 REM boost
@@ -140,67 +163,30 @@ set BOOST_SHARED_LIB_FLAGS= link=shared
 set BOOST_STATIC_LIB_FLAGS= link=static
 
 IF %WITH_BOOST% EQU 1 (
-    IF EXIST %DEPENDENCY_DIR%\boost_1_68_0 (
-        echo Found boost
+    IF %PLATFORM% EQU android (
+        echo === Downloading boost for android ===
+        powershell -Command "[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; Invoke-WebRequest https://github.com/dec1/Boost-for-Android/releases/download/ndk_19c_boost_1.69.0/boost_1.69.0_prebuilt_ndk_19c.zip -OutFile %DEPENDENCY_DIR%\boost_1.69.0_prebuilt_ndk_19c.zip"
+        7z x -y -bd -bb0 -aoa %DEPENDENCY_DIR%\boost_1.69.0_prebuilt_ndk_19c.zip -o%DEPENDENCY_DIR%
+        echo Copying boost to %DEPENDENCY_DIR%\install...
+        xcopy /E /q %DEPENDENCY_DIR%\boost_1.69.0_prebuilt_ndk_19 %DEPENDENCY_DIR%\install
+        echo Cleaning up boost files...
+        rmdir /S /q %DEPENDENCY_DIR%\boost_1.69.0_prebuilt_ndk_19
+        del %DEPENDENCY_DIR%\boost_1.69.0_prebuilt_ndk_19c.zip
     ) ELSE (
-        echo === Downloading boost ===
-        powershell -Command "Invoke-WebRequest http://dl.bintray.com/boostorg/release/1.68.0/source/boost_1_68_0.zip -OutFile %DEPENDENCY_DIR%\boost_1.68.0.zip"
-        7z x -y -bd -bb0 -o%DEPENDENCY_DIR% %DEPENDENCY_DIR%\boost_1.68.0.zip
-        mkdir %DEPENDENCY_DIR%\boost_1_68_0\build
+        IF EXIST %DEPENDENCY_DIR%\boost_1_69_0 (
+            echo Found boost
+        ) ELSE (
+            echo === Downloading boost ===
+            powershell -Command "Invoke-WebRequest http://dl.bintray.com/boostorg/release/1.69.0/source/boost_1_69_0.zip -OutFile %DEPENDENCY_DIR%\boost_1.69.0.zip"
+            7z x -y -bd -bb0 -o%DEPENDENCY_DIR% %DEPENDENCY_DIR%\boost_1.69.0.zip
+        )
+        echo === Building boost ===
+        pushd %DEPENDENCY_DIR%\boost_1_69_0
+        call %DEPENDENCY_DIR%\boost_1_69_0\bootstrap.bat
+        call %DEPENDENCY_DIR%\boost_1_69_0\b2.exe stage %BOOST_LIBS% %BOOST_SHARED_LIB_FLAGS% %BOOST_COMMON_FLAGS%
+        call %DEPENDENCY_DIR%\boost_1_69_0\b2.exe install %BOOST_LIBS% %BOOST_SHARED_LIB_FLAGS% %BOOST_COMMON_FLAGS%
+        call %DEPENDENCY_DIR%\boost_1_69_0\b2.exe stage %BOOST_LIBS% %BOOST_STATIC_LIB_FLAGS% %BOOST_COMMON_FLAGS%
+        call %DEPENDENCY_DIR%\boost_1_69_0\b2.exe install %BOOST_LIBS% %BOOST_STATIC_LIB_FLAGS% %BOOST_COMMON_FLAGS%
+        popd
     )
-    echo === Building boost ===
-    pushd %DEPENDENCY_DIR%\boost_1_68_0
-    call %DEPENDENCY_DIR%\boost_1_68_0\bootstrap.bat
-    call %DEPENDENCY_DIR%\boost_1_68_0\b2.exe stage %BOOST_LIBS% %BOOST_SHARED_LIB_FLAGS% %BOOST_COMMON_FLAGS%
-    call %DEPENDENCY_DIR%\boost_1_68_0\b2.exe install %BOOST_LIBS% %BOOST_SHARED_LIB_FLAGS% %BOOST_COMMON_FLAGS%
-    call %DEPENDENCY_DIR%\boost_1_68_0\b2.exe stage %BOOST_LIBS% %BOOST_STATIC_LIB_FLAGS% %BOOST_COMMON_FLAGS%
-    call %DEPENDENCY_DIR%\boost_1_68_0\b2.exe install %BOOST_LIBS% %BOOST_STATIC_LIB_FLAGS% %BOOST_COMMON_FLAGS%
-    popd
 )
-
-
-REM swig
-set SWIG_VER=swigwin-3.0.12
-IF EXIST %INSTALL_PREFIX%\swig (
-    echo Found SWIG
-) ELSE (
-    echo === Downloading SWIG === 
-    powershell -Command "Invoke-WebRequest https://phoenixnap.dl.sourceforge.net/project/swig/swigwin/swigwin-3.0.12/%SWIG_VER%.zip -OutFile %DEPENDENCY_DIR%\%SWIG_VER%.zip"
-    
-    echo === Unzipping swig === 
-    7z x -y -bd -bb0 -o%DEPENDENCY_DIR% %DEPENDENCY_DIR%\%SWIG_VER%.zip
-    echo Moving %DEPENDENCY_DIR%\%SWIG_VER% to %INSTALL_PREFIX%\swig
-    move "%DEPENDENCY_DIR%\%SWIG_VER%" "%INSTALL_PREFIX%\swig"
-)
-
-
-REM Unity
-SET UNITY_EXE="%DEPENDENCY_DIR%\unity\Editor\Unity.exe"
-SET UNITY_URL=https://netstorage.unity3d.com/unity/1a9968d9f99c/Windows64EditorInstaller/UnitySetup64-2018.2.1f1.exe
-IF %WITH_UNITY% EQU 1 (
-    IF EXIST %DEPENDENCY_DIR%\UnitySetup64.exe (
-        echo Found Unity
-    ) ELSE (
-        echo === Downloading Unity === 
-        powershell -Command "Invoke-WebRequest $env:UNITY_URL -OutFile %DEPENDENCY_DIR%\UnitySetup64.exe"
-    )
-
-    echo === Installing Unity === 
-    mkdir "%DEPENDENCY_DIR%\unity"
-    %DEPENDENCY_DIR%\UnitySetup64.exe /S /D="%DEPENDENCY_DIR%\unity"
-    del "%DEPENDENCY_DIR%\UnitySetup64.exe"
-)
-
-
-REM rtmidi
-IF EXIST %DEPENDENCY_DIR%\rtmidi\build (
-    echo Found RtMidi
-) ELSE (
-    echo === Cloning RtMidi ===
-    git clone https://github.com/thestk/rtmidi.git %DEPENDENCY_DIR%\rtmidi
-    mkdir "%DEPENDENCY_DIR%\rtmidi\build"
-)
-
-echo === Building RtMidi ===
-%CMAKE_BIN% -H"%DEPENDENCY_DIR%\rtmidi" -B"%DEPENDENCY_DIR%\rtmidi\build" %COMMON_GENERATOR_FLAGS% -DBUILD_TESTING=OFF
-%CMAKE_BIN% --build "%DEPENDENCY_DIR%\rtmidi\build" %COMMON_BUILD_FLAGS%
