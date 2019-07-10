d7a789e8440e7bb39ede15185b74e028df4de321
Byron Mallett <byronated@gmail.com>
2019-07-10 17:21:35 +1200

    Added Mac container. Now copying files out of VM using scp

--- containers/windows/build_showtime.bat
+++ containers/windows/build_showtime.bat
@@ -7,17 +7,3 @@ set CMAKE_FLAGS=%CMAKE_FLAGS% -A x64 -DBUILD_SHARED=ON -DBUILD_STATIC=ON -DBUILD
 echo Configure and make project
 cmake -H"%SOURCE_DIR%" -B"%BUILD_FOLDER%" -G "Visual Studio 16 2019" %CMAKE_FLAGS%
 cmake --build %BUILD_FOLDER% --config Release --target ALL_BUILD
-
-IF NOT EXIST %OUTPUT_FOLDER%\bin\windows (
-    mkdir "%OUTPUT_FOLDER%\bin\windows"
-)
-
-IF NOT EXIST %OUTPUT_FOLDER%\lib\windows (
-    mkdir "%OUTPUT_FOLDER%\lib\windows"
-)
-
-echo Copying binaries to %OUTPUT_FOLDER%\bin\windows
-xcopy /S /Y /E %BUILD_FOLDER%\bin %OUTPUT_FOLDER%\bin\windows
-
-echo Copying libraries to %OUTPUT_FOLDER%\lib\windows
-xcopy /S /Y /E %BUILD_FOLDER%\lib %OUTPUT_FOLDER%\lib\windows
