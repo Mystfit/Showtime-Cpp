f218dcecfbde5b41dcde03c07aa2515e0849c6e2
Byron Mallett <byronated@gmail.com>
2019-07-25 22:28:18 +1200

    Nuget package generation working (tested on OSX). Will bundle runtime files for windows and mac

--- containers/android/build_singularity_android.sh
+++ containers/android/build_singularity_android.sh
@@ -1,1 +1,1 @@
-singularity run -B $SHOWTIME_SOURCE:/showtime-source,$SHOWTIME_OUTPUT:/showtime-output,$SHOWTIME_BUILD:/showtime-build $VM_DIR/build-android.sif
+singularity run -B $SHOWTIME_SOURCE:/showtime-source,$SHOWTIME_BIN_OUTPUT:/showtime-bin-output,$SHOWTIME_LIB_OUTPUT:/showtime-lib-output,$SHOWTIME_BUILD:/showtime-build $VM_DIR/build-android.sif
