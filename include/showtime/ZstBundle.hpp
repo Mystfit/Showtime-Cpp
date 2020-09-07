5398e93d7c3c38490f41f685b71be7213dc31d09
Byron Mallett <byronated@gmail.com>
2020-09-07 20:36:30 +1200

    Removed ZST_EXPORT from ZstBundle

--- include/showtime/ZstBundle.hpp
+++ include/showtime/ZstBundle.hpp
@@ -10,22 +10,22 @@ namespace showtime {
 	class ZstBundleIterator
 	{
 	public:
-        ZST_EXPORT ZstBundleIterator(typename std::vector<T>::iterator it) : m_it(it){
+        ZstBundleIterator(typename std::vector<T>::iterator it) : m_it(it){
         }
         
-        ZST_EXPORT bool operator!=(const ZstBundleIterator& other){
+        bool operator!=(const ZstBundleIterator& other){
             return (m_it != other.m_it);
         }
         
-        ZST_EXPORT bool operator==(const ZstBundleIterator& other){
+        bool operator==(const ZstBundleIterator& other){
             return (m_it == other.m_it);
         }
         
-        ZST_EXPORT T operator*() const{
+        T operator*() const{
             return *m_it;
         }
         
-        ZST_EXPORT const ZstBundleIterator& operator++(){
+        const ZstBundleIterator& operator++(){
             m_it++;
             return *this;
         }
@@ -43,40 +43,40 @@ namespace showtime {
 	class ZstBundle
 	{
 	public:
-        ZST_EXPORT ZstBundle(){};
-        ZST_EXPORT ~ZstBundle(){};
-        ZST_EXPORT ZstBundle(const ZstBundle& other){
+        ZstBundle(){};
+        ~ZstBundle(){};
+        ZstBundle(const ZstBundle& other){
             m_bundle_items = other.m_bundle_items;
         };
-        ZST_EXPORT ZstBundle(ZstBundle&& other){
+        ZstBundle(ZstBundle&& other){
             m_bundle_items = std::move(other.m_bundle_items);
             other.m_bundle_items.clear();
         }
 
-        ZST_EXPORT ZstBundleIterator<T> begin(){
+        ZstBundleIterator<T> begin(){
             return ZstBundleIterator<T>(m_bundle_items.begin());
         }
-        ZST_EXPORT ZstBundleIterator<T> end(){
+        ZstBundleIterator<T> end(){
             return ZstBundleIterator<T>(m_bundle_items.end());
         }
-        ZST_EXPORT void add(T bundle_item){
+        void add(T bundle_item){
             m_bundle_items.emplace_back(bundle_item);
         }
 
-        ZST_EXPORT T item_at(const size_t index) const{
+        T item_at(const size_t index) const{
             if (index >= m_bundle_items.size()) {
                 throw std::out_of_range("Bundle index out of range");
             }
             return m_bundle_items[index];
         }
 
-        ZST_EXPORT size_t size() const{
+        size_t size() const{
             return m_bundle_items.size();
         }
-        ZST_EXPORT T operator[](const size_t& index) const{
+        T operator[](const size_t& index) const{
             return item_at(index);
         }
-        ZST_EXPORT void clear(){
+        void clear(){
             m_bundle_items.clear();
         }
 
